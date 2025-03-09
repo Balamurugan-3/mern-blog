fe3a662bea524e606a24dd5d7349561614c67788
Balamurugan-3 <bala14096@gmail.com>
2025-03-09 08:46:42 +0530

    fresh its started to deploy on render added .gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,3 @@
-// import express from 'express'
-// import dotenv from "dotenv"
-// import cookieParser from 'cookie-parser'
-// import cors from "cors"
-// import connectDatabase from "./db/connectDatabase.js"
-// // import routes
-// import authRoute from "./routes/auth.route.js"
-// import userRoute from "./routes/user.route.js"
-// import categoryRoute from "./routes/category.route.js"
-// import blogRoute from "./routes/blog.route.js"
-// import commentRoute from "./routes/comment.route.js"
-// import blogLikeRoute from "./routes/blogLike.route.js"
-
-// const app = express()
-
-// dotenv.config()
-// app.use(express.json())
-// app.use(cookieParser())
-// // app.use(cors({ origin: "https://mern-blog-ury1.vercel.app", credentials: true }))
-// app.use(
-//     cors({
-//         origin: "https://mern-blog-ury1.vercel.app",
-//         credentials: true,
-//         methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-//         allowedHeaders: "Content-Type,Authorization",
-//     })
-// );
-
-// // api end points
-// app.use("/api/auth", authRoute)
-// app.use("/api/user", userRoute)
-// app.use("/api/category", categoryRoute)
-// app.use("/api/blog", blogRoute)
-// app.use("/api/comment", commentRoute)
-// app.use("/api/blogLike", blogLikeRoute)
-
-// app.listen(process.env.PORT, () => {
-//     console.log(`Server Running Port ${process.env.PORT} in ${process.env.NODE_ENV}`)
-//     connectDatabase()
-// })
-
 import express from 'express'
 import dotenv from "dotenv"
 import cookieParser from 'cookie-parser'
@@ -63,10 +22,8 @@ app.use(cookieParser())
 // ✅ Updated CORS Configuration
 app.use(
     cors({
-        origin: ["https://mern-blog-ury1.vercel.app", "http://localhost:3000"],
+        origin: ["https://mern-blog-ury1.vercel.app", "http://localhost:5173"],
         credentials: true,
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        allowedHeaders: "Content-Type,Authorization",
     })
 );
 
